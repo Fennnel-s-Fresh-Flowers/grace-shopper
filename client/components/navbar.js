1e08d0b2fd5f09f74d848fe68534c3def3037098
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-14 18:04:44 -0500

    css for the evening

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -8,17 +8,20 @@ const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <h1>FENNEL'S FRESH FLOWERS</h1>
     <nav id="nav-bar">
-      <Link to="/home">Home</Link>
-      <Link to="/flowers">Flowers</Link>
+      <div className="nav-pair">
+        <Link to="/home">Home</Link>
+        <Link to="/flowers">Flowers</Link>
+      </div>
       {isLoggedIn ? (
-        <div>
+        <div className="nav-pair">
           {/* The navbar will show these links after you log in */}
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          <a>Settings</a>
         </div>
       ) : (
-        <div>
+        <div className="nav-pair">
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
