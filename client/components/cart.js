8630af1d48dd5b297403b219d3fb7c16c87d8624
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-14 15:15:25 -0500

    add div containers in cart.js

--- client/components/cart.js
+++ client/components/cart.js
@@ -3,18 +3,22 @@ import React from 'react'
 class Cart extends React.Component {
   render() {
     return (
-      <section className="cart">
-        <ul>
-          <li>imageUrl</li>
-          <li>name</li>
-          <li>quantity</li>
-          <li>itemPrice</li>
-          <li>itmeTotal</li>
-          <button type="button">EditCart</button>
-        </ul>
-        <div>cartTotal</div>
-        <button type="button">CHECKOUT</button>
-      </section>
+      <div className="cart">
+        <section>
+          <div>
+            <ul>
+              <li>imageUrl</li>
+              <li>name</li>
+              <li>quantity</li>
+              <li>itemPrice</li>
+              <li>itmeTotal</li>
+              <button type="button">EditCart</button>
+            </ul>
+          </div>
+          <div>cartTotal</div>
+          <button type="button">CHECKOUT</button>
+        </section>
+      </div>
     )
   }
 }
