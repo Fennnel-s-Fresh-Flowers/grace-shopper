513016f20b5f133dd2d853c7634213570e32bdd6
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-14 15:49:07 -0500

    add flexbox nav

--- client/components/cart.js
+++ client/components/cart.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Cart extends React.Component {
   render() {
     return (
-      <div className="cart">
+      <div id="cart">
         <section>
           <div>
             <ul>
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
+  <div id="nav-bar">
     <h1>FENNEL'S FRESH FLOWERS</h1>
     <nav>
       <Link to="/home">Home</Link>
