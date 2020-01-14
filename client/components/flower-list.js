16607cd523bf34d7e99e7acbadcae6895b4af2cc
PeterCrosta <pscrosta@gmail.com>
2020-01-14 15:18:08 -0500

    changed all product to flower

---
+++ client/components/flower-list.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function FlowerList(props) {
+  return (
+    <div className="flower-list">
+      {props.flowers.map(flower => (
+        <div key={flower.id}>
+          <Link to={`/flowers/:${flower.id}`}>
+            <img srg={flower.imageUrl} />
+            <h3>{flower.name}</h3>
+          </Link>
+          <div>${flower.price / 100}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
