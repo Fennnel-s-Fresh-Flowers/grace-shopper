dd79033dcf5aa5617803a9212d3a4e058f0d2cb9
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-14 11:24:00 -0500

    add user/:id get

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,16 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.find({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
