4374fbfec01594e3de97bfb7d49d6de50b5aeb82
Erik Jenson <erik.a.jenson@gmail.com>
2020-01-14 23:49:52 -0500

    feat: add cart items from store to cart component, feat: add dispatch items to store in SingleFlower component, chore: edit user reducer to manage these state changes, fix: delete SingleCartItem component, test: tried but failed to change main layout in css

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,26 +1,39 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 class Cart extends React.Component {
   render() {
+    const items = this.props.cartItems
     return (
       <div id="cart">
         <section>
-          <div>
-            <ul>
-              <li>imageUrl</li>
-              <li>name</li>
-              <li>quantity</li>
-              <li>itemPrice</li>
-              <li>itmeTotal</li>
+          <h3>CART</h3>
+          {!items.length ? (
+            <span>Your Cart Is Empty</span>
+          ) : (
+            <div>
+              {items.map(item => (
+                <div key={item.id}>
+                  <li>{`Item: ${item.name}`}</li>
+                  <li>{`Quantity: ${item.quantity}`}</li>
+                </div>
+              ))}
+
+              <h4>Total: ${items.reduce((a, i) => a + i.price, 0) / 100}</h4>
+
               <button type="button">EditCart</button>
-            </ul>
-          </div>
-          <div>cartTotal</div>
-          <button type="button">CHECKOUT</button>
+
+              <button type="button">CHECKOUT</button>
+            </div>
+          )}
         </section>
       </div>
     )
   }
 }
 
-export default Cart
+const mapStateToProps = state => {
+  return {cartItems: state.user.cart}
+}
+
+export default connect(mapStateToProps, null)(Cart)
