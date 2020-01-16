1ed7bf2658a188a7ddf7d076a4a720c5f57dbd73
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-16 12:37:34 -0500

    add selforadmin rout protector, add basics for loggin rout protection, currently commented out, needs passport auth

--- server/api/routProtection.js
+++ server/api/routProtection.js
@@ -13,3 +13,21 @@ function isSelf(req, res, next) {
     res.redirect('/login')
   }
 }
+
+function isSelfOrAdmin(req, res, next) {
+  if (req.params.id == req.user.id || req.user.admin) {
+    return next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+//Need to write a loggedin function that has to do with authentication of user
+// function isLoggedin(req, res, next) {
+//   if(loggedin) { //passport auth?
+//     return next()
+//   } else {
+//     res.redirect('/')
+//   }
+
+// }
