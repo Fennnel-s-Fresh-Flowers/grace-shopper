cd7023ab73ecfe9e1005e72f5493f5cec2336d27
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-16 20:04:45 -0500

    add css styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>FENNEL'S FRESH FLOWERS</h1>
+    <h1 id="title">FENNEL'S FRESH FLOWERS</h1>
     <nav id="nav-bar">
       <div className="nav-pair">
         <Link to="/home">Home</Link>
