9ebb563a8b6001934129b676adbad0d79d9203e6
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-18 13:13:37 -0500

    feature: navbar hr updated. swapped nav to top of page

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,9 +6,6 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1 id="title">
-      <div id="title-text">FENNEL'S FRESH FLOWERS</div>
-    </h1>
     <nav id="nav-bar">
       <div className="nav-pair">
         <Link to="/home">Home</Link>
@@ -24,13 +21,16 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         </div>
       ) : (
         <div className="nav-pair">
-          {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          {/* The navbar will show these links before you log in */}
         </div>
       )}
     </nav>
-    <hr />
+    <hr id="nav-hr" />
+    <h1 id="title">
+      <div id="title-text">FENNEL'S FRESH FLOWERS</div>
+    </h1>
   </div>
 )
 
