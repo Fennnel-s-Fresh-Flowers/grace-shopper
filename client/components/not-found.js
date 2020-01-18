67dc4a4c6fa85ea83dd2abdac630f13dac0fa710
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-18 18:04:44 -0500

    fix: temp fix to error page, still have jpg issue

--- client/components/not-found.js
+++ client/components/not-found.js
@@ -3,10 +3,13 @@ import {Link} from 'react-router-dom'
 // import errorImg from '../../public/bundle/errorPage.jpg' //currently having webpack issues with this
 const NotFound = () => (
   <div id="error-page">
-    <center>Oops! Page Not Found!</center>
-    <img src="../../public/errorPage.jpg" />
     <center>
-      <Link to="/home">Return to Home Page</Link>
+      <center>Oops! Page Not Found!</center>
+      {/* <img src="../../public/errorPage.jpg" /> */}
+      <img src="https://i.pinimg.com/originals/7b/73/02/7b7302edb75cc113058470340418d9ef.jpg" />
+      <center>
+        <Link to="/home">Return to Home Page</Link>
+      </center>
     </center>
   </div>
 )
