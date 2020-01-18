65545d2a2af7a2a48e75777e3504e588d6d82191
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-18 17:59:40 -0500

    feature: back to home button on 404 page~~ jpg image still broken.

--- client/components/not-found.js
+++ client/components/not-found.js
@@ -3,9 +3,10 @@ import {Link} from 'react-router-dom'
 // import errorImg from '../../public/bundle/errorPage.jpg' //currently having webpack issues with this
 const NotFound = () => (
   <div id="error-page">
+    <center>Oops! Page Not Found!</center>
     <img src="../../public/errorPage.jpg" />
     <center>
-      <Link to="/">Return to Home Page</Link>
+      <Link to="/home">Return to Home Page</Link>
     </center>
   </div>
 )
