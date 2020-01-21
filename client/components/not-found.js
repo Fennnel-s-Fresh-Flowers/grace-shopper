7f4f33c484eb8664272012e1112c7f52a2a7446e
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-21 18:49:38 -0500

    feature: rely on local file for 404 error

--- client/components/not-found.js
+++ client/components/not-found.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-// import errorImg from '../../public/bundle/errorPage.jpg' //currently having webpack issues with this
+
 const NotFound = () => (
   <div id="error-page">
     <center>
       <center>Oops! Page Not Found!</center>
       {/* <img src="../../public/errorPage.jpg" /> */}
-      <img src="https://i.pinimg.com/originals/7b/73/02/7b7302edb75cc113058470340418d9ef.jpg" />
+      <img src="/errorPage.jpg" />
       <center>
         <Link to="/home">Return to Home Page</Link>
       </center>
