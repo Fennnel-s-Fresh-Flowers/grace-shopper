38ebeb7298efe2ab82bc7397872c3f3c005dcfb7
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-21 11:54:21 -0500

    messing with oauth

--- server/auth/google.js
+++ server/auth/google.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const {User} = require('../db/models')
-// const {process} = require('../../secrets')
+// const {ID, SECRET, CALLBACK} = require('../../secrets')
 module.exports = router
 
 /**
