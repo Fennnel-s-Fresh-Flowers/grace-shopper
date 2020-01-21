6162639e70b180dce95a7f40e9276328d8bda104
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-21 16:18:12 -0500

    remove a small redundancey in test

--- client/components/o-auth-login.js
+++ client/components/o-auth-login.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OauthLoginForm = props => {
   return (
-    <form method="get" action="/auth/google">
+    <form method="get" action="server/auth/google">
       <button type="submit" className="o-auth-button">
         Login with Google
       </button>
