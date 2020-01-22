95238f53c9e0db2f0566bbe31df37b2dfc239d80
PeterCrosta <pscrosta@gmail.com>
2020-01-22 18:48:05 -0500

    now myAccount page is generating past orders

--- client/components/order-list.js
+++ client/components/order-list.js
@@ -1,3 +1,42 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-export default function PastOrders(props) {}
+export default function PastOrders(props) {
+  console.log('past orders list generator. props: ', props)
+  const {orders} = props
+  //this should generate past orders from the user by mapping though the orders, mapping through the eager-loaded array of contents of the order
+  return (
+    <div>
+      <h2>Your past orders:</h2>
+      <ul>
+        {orders.map(order => {
+          const date = order.updatedAt.slice(0, 10)
+          return (
+            <div key={order.id}>
+              <li>
+                <h3>Ordered on {date}</h3>
+                <p>Total: ${order.total / 100}</p>
+                <ul>
+                  {order.flowers.map(flower => {
+                    return (
+                      <div key={flower.id}>
+                        <li>
+                          <Link to={`/flowers/${flower.id}`}>
+                            <h4>
+                              {flower.orderFlower.quantity} {flower.name}s
+                            </h4>
+                            <img className="order-images" src={flower.imgUrl} />
+                          </Link>
+                        </li>
+                      </div>
+                    )
+                  })}
+                </ul>
+              </li>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
